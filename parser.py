db10228152a2f2eb053644e7c1b2cb8a4306a8f2
fabio-biot <chaput.fabio@gmail.com>
2026-05-22 11:28:05 +0200

    siu

--- parser.py
+++ parser.py
@@ -1,5 +1,4 @@
-from models import StartHub, EndHub, Hub, Drone, Connection, Parser
-
+from models import StartHub, Pathfinder
 
     
 def params(data):
@@ -8,8 +7,16 @@ def params(data):
         
 
 def main():
-    parser = Parser("maps/easy/01_linear_path.txt")
+    parser = Parser("maps/easy/02_simple_fork.txt")
     network, nb_drones = parser.parse()
+    start_hub = network.start_hub
+    end_hub = network.end_hub
+    path_finder = Pathfinder()
+    pathin = path_finder.find_path(start=start_hub, end=end_hub)
+    for path in pathin:
+        print(path.name)
+    print(f"Start {start_hub.name}")
+    print(f"End {end_hub.connections[0].hub1.name}")
     print(f"network {network}")
     for i in  range(len(network.connections)):
         print(f"Connection {i} = ")
